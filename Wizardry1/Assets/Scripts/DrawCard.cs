5adf789c1d5685d8b2d986979190a871ec3800c1
Moritz <m.p.schick@web.de>
2021-05-16 15:08:47 +0200

    video8

---
+++ Wizardry1/Assets/Scripts/DrawCard.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class DrawCard : NetworkBehaviour
+{
+    public PlayerManager PlayerManager;
+    public void OnClick()
+    {
+        NetworkIdentity networkIdentity = NetworkClient.connection.identity;
+        PlayerManager = networkIdentity.GetComponent<PlayerManager>();
+        PlayerManager.CmdDealCards();
+    }
+}
